b0c22cebeedb9c8436f1ab2739e7e6350c52ffe0
kingsuk-panda <kingsuk.panda.2004@gmail.com>
2025-05-11 09:49:47 +0000

    Modified client/src/components/Navbar.js to use CSS module

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css'; // Import the CSS module
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li>
-          {localStorage.getItem('user') ? ( // Check if user is logged in (example)
-            <Link to="/account">Account</Link>
+    <nav className={styles.nav}>  {/* Apply the nav style */}
+      <ul className={styles.ul}>   {/* Apply the ul style */}
+        <li className={styles.li}><Link to="/" className={styles.a}>Home</Link></li>  {/* Apply the li and a styles */}
+        <li className={styles.li}><Link to="/products" className={styles.a}>Products</Link></li>
+        <li className={styles.li}><Link to="/about" className={styles.a}>About</Link></li>
+        <li className={styles.li}>
+          {localStorage.getItem('user') ? (
+            <Link to="/account" className={styles.a}>Account</Link>
           ) : (
-            <Link to="/login">Login / Sign Up</Link>
+            <Link to="/login" className={styles.a}>Login / Sign Up</Link>
           )}
         </li>
       </ul>
