628df3aaa6770c5c2fe25412c5f263c4c8bb047d
kingsuk-panda <kingsuk.panda.2004@gmail.com>
2025-05-11 11:14:31 +0000

    Improved colour palette (App.js), better typography (index.html), applied fonts in App.css, enhanced navbar styling (Navbar.module.css), added some padding to the main content area (App.css)

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+// client/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
@@ -9,21 +10,24 @@ import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
 import About from './pages/About'; // Added Import
 import Navbar from './components/Navbar'; // Import the Navbar component
+import './App.css'; // Import the app-wide styles
 
 function App() {
   return (
     <Router>
-      <Navbar /> {/* Include the Navbar on every page */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:productId" element={<ProductPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/about" element={<About />} /> {/* Added Route */}
-      </Routes>
+      <Navbar />
+      <div className="container"> {/* Add the container here */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<ProductPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/about" element={<About />} /> {/* Added Route */}
+        </Routes>
+      </div> {/* Closing tag for container */}
     </Router>
   );
 }
