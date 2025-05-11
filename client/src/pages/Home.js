f5b3d4935674150b891153131dc04ee794123827
kingsuk-panda <kingsuk.panda.2004@gmail.com>
2025-05-11 15:03:18 +0000

    Updated Home.js, Renders HeroSection at the top, then a container for other content

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,23 @@
+// client/src/pages/Home.js
 import React from 'react';
 import HeroSection from '../components/HeroSection';
-// import styles from './Home.module.css'; // if using styling
+// import styles from './Home.module.css'; // Assuming Home.module.css might not be needed or is minimal
 
 function Home() {
   return (
-    <div>
+    <div> {/* This div will allow HeroSection to be full width/height before the container */}
       <HeroSection />
-      {/* Other content for the Home page will go here later */}
+      {/* Content below the hero section will go into a standard container */}
+      <div className="container page-content-after-hero"> {/* Use a general or specific class for spacing */}
+        <h2>Welcome to Our Plant Paradise!</h2>
+        <p>
+          Explore our collections, find plant care tips, or get in touch with us.
+          We are here to help you bring more green into your life.
+        </p>
+        {/* You can add more sections here like: */}
+        {/* <FeaturedProducts /> */}
+        {/* <CareTipsPreview /> */}
+      </div>
     </div>
   );
 }
