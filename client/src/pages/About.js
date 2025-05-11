1f7532649061ee645cc532fab2c3f0e8bdd187c9
kingsuk-panda <kingsuk.panda.2004@gmail.com>
2025-05-11 10:01:29 +0000

    Added client/src/pages/About.js and updated content within it

---
+++ client/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our plant nursery! We are passionate about plants and dedicated to providing high-quality plants and exceptional service.</p>
+      {/* Add more information about your nursery here */}
+    </div>
+  );
+}
+
+export default About;
